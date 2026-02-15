25db503fb348e77727b6e73279ffacf72ca82bdb
GodsplanElot <elotgodsplan2@gmail.com>
2026-02-15 13:16:10 +0100

    Update Home component: add description paragraph for Promptopa tool

--- app/page.jsx
+++ app/page.jsx
@@ -8,6 +8,9 @@ const Home = () => {
             <br className="max-md:hidden" />
             <span className="orange_gradient text-center">AI-Powered Prompts</span>
         </h1>
+        <p>
+            Promptopa is an open -source AI prompting tool for modern world to discover, create and share creative prompts
+        </p>
     </section>
   )
 }
