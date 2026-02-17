26fffef5ff04e07e0ee7abbe9336a1400a654ba9
GodsplanElot <elotgodsplan2@gmail.com>
2026-02-17 13:46:47 +0100

    feat: update Provider component to include session management with SessionProvider

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+"use client"
+import { SessionProvider } from '/next-auth/react'
 
-const Provider = () => {
+const Provider = ({ children, session }) => {
   return (
     <div>
-      Provider
+      <SessionProvider session={session}>
+        {children}
+      </SessionProvider>
     </div>
   )
 }
