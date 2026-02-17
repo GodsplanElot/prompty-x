1c501372da34c20d1fe117881bbd0fdff023493a
GodsplanElot <elotgodsplan2@gmail.com>
2026-02-17 14:09:37 +0100

    feat: add NextAuth configuration with Google provider for authentication

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,6 +12,7 @@ const RootLayout = ({ children }) => {
   return (
 <html lang="en">
     <body>
+        <provider>
         <div className='main'>
             <div className='gradient' />
         </div>
@@ -20,6 +21,7 @@ const RootLayout = ({ children }) => {
             <Nav />
             {children}
         </main>
+        </provider>
     </body>
 </html>
   )
